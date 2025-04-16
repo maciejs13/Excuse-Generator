55a4d05f66778ad2c67ddd37806d819460eb175f
Maciej Śledziński <maciejs.sledzinski@cyfroweszkoly.pl>
2025-04-16 13:55:54 +0200

    t

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,47 @@
-import './App.css'
-import ExcuseGenerator from "./ExcuseGenerator.tsx";
+import './App.css';
+import FetchExcuses from './FetchExcuses';
+import ExcuseShow from './ExcuseShow';
+import { useState } from 'react';
 
-function App() {
-
-  return (
-    <>
-        <div>
-
-          <h1>Wyszukaj wymówkę:</h1>
-          <ExcuseGenerator />
-
-
-        </div>
-    </>
-  )
+export interface Excuse {
+    id: number;
+    name: string;
+    reason: string;
+    credibility: number;
+    date: string;
+    creativity: string;
+    details: string;
+    urgent: boolean;
 }
 
-export default App
+function App() {
+    const storedExcuses = localStorage.getItem('excuses');
+    const [excuses, setExcuses] = useState<Excuse[]>(storedExcuses ? JSON.parse(storedExcuses) : []);
+
+    const updateStorage = (newExcuses: Excuse[]) => {
+        setExcuses(newExcuses);
+        localStorage.setItem('excuses', JSON.stringify(newExcuses));
+    };
+
+    const addExcuse = (excuse: Excuse) => {
+        const newExcuses = [...excuses, excuse];
+        updateStorage(newExcuses);
+    };
+
+    const deleteExcuse = (id: number) => {
+        const newExcuses = excuses.filter(e => e.id !== id);
+        updateStorage(newExcuses);
+    };
+
+
+    return (
+        <div className="app-container">
+            <h1>Generator Wymówek</h1>
+            <FetchExcuses onAdd={addExcuse} />
+            <ExcuseShow excuses={excuses} onDelete={deleteExcuse} />
+
+        </div>
+    );
+}
+
+export default App;
